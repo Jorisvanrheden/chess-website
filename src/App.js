b674d2afcf6bd0e6abe02674e02784036ec853f1
Joris van Rheden <j.vanrheden@triboform.com>
2021-03-23 23:36:32 +0100

    board squares

--- src/App.js
+++ src/App.js
@@ -1,7 +1,75 @@
+import { Component } from "react";
+import React from "react";
+
+class BoardSquare extends Component
+{
+  constructor(props)
+  {
+    super(props);
+  }
+
+  getStyling(index)
+  {
+    if(index % 2 === 0) return "squarelight";
+    return "squaredark";
+  }
+
+  render()
+  {
+    return(
+      <img 
+        className={this.getStyling(this.props.x + this.props.y)}
+        onClick={()=>{this.props.clickSquare(this.props.x, this.props.y)}}
+        src="queen_white.png"
+      >
+      </img>
+    );
+  }
+}
+
+class ChessBoard extends Component
+{
+  constructor(props)
+  {
+    super(props);
+  }
+
+  
+
+  clickSquare(x, y)
+  {
+    console.log("Clicked the tile at: " + x + ", " + y);
+  }
+
+  render()
+  {
+    const rows = [1,2,3,4,5,6,7,8];
+    const columns = [1,2,3,4,5,6,7,8];
+
+    var elem = document.getElementById('squarelight');
+    console.log(elem);
+    //elem.style.width = 70 + "%";
+
+    return(
+      <div>        
+        {
+          rows.map((value, rowIndex) => 
+          (
+            columns.map((value, columnIndex) => 
+            (
+              <BoardSquare clickSquare={this.clickSquare} x={rowIndex} y={columnIndex}/>
+            ))
+          ))
+        }
+      </div>   
+    );
+  }
+}
+
 function App() {
   return (
     <div>
-     Hello World!
+      <ChessBoard/>
     </div>
   );
 }
